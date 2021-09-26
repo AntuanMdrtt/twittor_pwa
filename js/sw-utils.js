e3a07d0e48344ff5f5ca3c3ce4c68ce08b722da3
Antonio Hernandez <ahernandezh.dev@gmail.com>
2021-09-25 21:49:57 -0500

    Primer commit, subir la aplicación a github

---
+++ js/sw-utils.js
@@ -0,0 +1,11 @@
+// Save dynamic cache
+function updateDynamicCache(dynamic_cache, req, res) {
+  if (res.ok) {
+    return caches.open(dynamic_cache).then((cache) => {
+      cache.put(req, res.clone());
+      return res.clone();
+    });
+  } else {
+    return res;
+  }
+}
